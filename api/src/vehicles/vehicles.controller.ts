3715d23068699bf8fb8f55cbb51c4eff6cc731ee
George Acquah <georgeacquah889@gmail.com>
2023-11-24 13:15:17 +0000

    modified vehicle logics

--- api/src/vehicles/vehicles.controller.ts
+++ api/src/vehicles/vehicles.controller.ts
@@ -3,13 +3,13 @@ import { AddVehicleDto } from './dtos/add-vehicle.dto';
 import { VehiclesService } from './vehicles.service';
 import { User } from 'src/shared/decorators/user.decorator';
 import { _ISanitizedCustomer } from 'src/shared/interfaces/users.interface';
-import { JwtAuthGuard } from 'src/shared/guards/Jwt.guard';
+import { VehicleAuthGuard } from 'src/shared/guards/vehicles.guard';
 
 @Controller('customer/vehicle')
 export class VehiclesController {
   constructor(private vehicleService: VehiclesService) {}
 
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(VehicleAuthGuard)
   @Post()
   async addVehicle(
     @Body() vehicleData: AddVehicleDto,
@@ -17,6 +17,10 @@ export class VehiclesController {
   ) {
     //TODO add new vehicle to customer vehicles
     const image = 'some-random-test-image'; // Image ID returned by cloud provider
-    return await this.vehicleService.addVehicle(customer, vehicleData, image);
+    return await this.vehicleService.addVehicle(
+      customer._id,
+      vehicleData,
+      image,
+    );
   }
 }
