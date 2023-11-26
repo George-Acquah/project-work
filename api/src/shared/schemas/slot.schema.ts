59d1869ef4bc96acd4a28df697a004e0c614df99
George Acquah <georgeacquah889@gmail.com>
2023-11-26 00:04:14 +0000

    create schemas for center and slot images and modified other schemas

--- api/src/shared/schemas/slot.schema.ts
+++ api/src/shared/schemas/slot.schema.ts
@@ -1,5 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+import { HydratedDocument, Schema as MongooseSchema } from 'mongoose';
+import { ParkingCenter } from './parking-centers.schema';
 
 export type SlotDocument = HydratedDocument<Slot>;
 @Schema()
@@ -8,22 +9,35 @@ export class Slot {
   type: string;
 
   @Prop({ type: String, required: true })
-  name: string;
+  slot_name: string;
 
   @Prop({ type: String, required: true })
   description: string;
 
-  @Prop({ type: String, required: false, default: null })
-  image: string;
-
   @Prop({ type: Boolean, default: false })
   isAvailable: boolean;
+
+  @Prop([
+    {
+      type: MongooseSchema.Types.ObjectId,
+      ref: 'ParkingCenter',
+      required: true,
+    },
+  ])
+  center_id: ParkingCenter;
 }
 
 const SlotSchema = SchemaFactory.createForClass(Slot);
 
 SlotSchema.virtual('slot_data', {
   ref: 'SlotData',
+  localField: '_id',
+  foreignField: 'slot_id',
+  justOne: true,
+});
+
+SlotSchema.virtual('slot_images', {
+  ref: 'SlotImage',
   localField: '_id',
   foreignField: 'slot_id',
   justOne: true,
