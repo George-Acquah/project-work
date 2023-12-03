bc2456d5acac6cdeb4bd0e9c3fea70fccd775b67
George Acquah <georgeacquah889@gmail.com>
2023-12-03 01:02:12 +0000

    Created unverified users routes for the dashboard which is used in viewing unverified users, error boundary page and a not found page

---
+++ client/src/admin/app/dashboard/users/unverified/page.tsx
@@ -0,0 +1,45 @@
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
+import { Suspense } from "react";
+import { Metadata } from "next";
+import { lusitana } from "@/app/ui/font";
+import SearchApplicants from "@/app/ui/users/search";
+
+export const metadata: Metadata = {
+  title: "Unverified Applicants",
+};
+
+interface ISearchParams {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}
+
+export default async function ApplicationsPage({
+  searchParams,
+}: ISearchParams) {
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page) || 1;
+
+  // const totalPages = await fetchInvoicesPages(query);
+
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>
+          Unverifed Applicants
+        </h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <SearchApplicants />
+        {/* <CreateInvoice /> */}
+      </div>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        {/* <Table query={query} currentPage={currentPage} /> */}
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        {/* <Pagination totalPages={totalPages} /> */}
+      </div>
+    </div>
+  );
+}
