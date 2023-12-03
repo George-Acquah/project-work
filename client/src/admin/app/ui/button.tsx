b3216991afe970c5b254d8fcf0b23ad4fae72e63
George Acquah <georgeacquah889@gmail.com>
2023-12-03 01:26:15 +0000

    created a button component and duplicated logos component

--- client/src/admin/app/ui/button.tsx
+++ client/src/admin/app/ui/button.tsx
@@ -30,7 +30,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     const buttonClasses = clsx(
       "inline-flex items-center justify-center rounded-md text-sm font-medium shadow ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 aria-disabled:cursor-not-allowed aria-disabled:opacity-50'",
       {
-        "bg-tremor-brand text-primary-foreground shadow-md hover:bg-tremor-brand-emphasis":
+        "shadow-md  bg-blue-500 hover:bg-blue-400 transition duration-20":
           variant === "default",
         "bg-btn-destructive text-destructive-foreground hover:bg-destructive/90":
           variant === "destructive",
