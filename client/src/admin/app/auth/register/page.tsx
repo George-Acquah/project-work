f61366389f494c6c21e12bf75ea4a1bb47740c9a
George Acquah <georgeacquah889@gmail.com>
2023-12-03 00:54:05 +0000

    Created Login and Register account pages

--- client/src/admin/app/auth/register/page.tsx
+++ client/src/admin/app/auth/register/page.tsx
@@ -2,15 +2,16 @@ import SmartCarParkingAdminLogo from "@/app/ui/logos";
 import LoginForm from "@/app/ui/login-form";
 import { Metadata } from "next";
 import { secondaryBg, strongTextColor } from "@/app/ui/themes";
+import RegistrationForm from "@/app/ui/register-user-form";
 
 export const metadata: Metadata = {
-  title: "Login",
+  title: "Sign Up",
 };
 
-export default function LoginPage() {
+export default function RegistrationPage() {
   return (
     <main className="flex items-center justify-center">
-      <div className="relative mx-auto flex w-full max-w-[450px] flex-col space-y-2.5 p-4 md:mt-4">
+      <div className="relative mx-auto flex w-full max-w-[460px] flex-col space-y-2.5 p-4 md:mt-4">
         <div
           className={`flex justify-content items-center h-24 w-full rounded-lg p-3 md:h-32 ${secondaryBg} ${strongTextColor}`}
         >
@@ -19,7 +20,7 @@ export default function LoginPage() {
           </div>
           <p className="uppercase text-2xl ml-4">Smart Car Parking Admin</p>
         </div>
-        <LoginForm />
+        <RegistrationForm />
       </div>
     </main>
   );
