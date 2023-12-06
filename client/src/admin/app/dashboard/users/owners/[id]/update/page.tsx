e9f32a31f2afb69f495190b72faa6f829709ca21
George Acquah <georgeacquah889@gmail.com>
2023-12-06 00:19:43 +0000

    Modified contents in different routes within dashboard

--- client/src/admin/app/dashboard/users/owners/[id]/update/page.tsx
+++ client/src/admin/app/dashboard/users/owners/[id]/update/page.tsx
@@ -1,44 +1,19 @@
-import { notFound } from "next/navigation";
-
 import { Metadata } from "next";
 import { dashboardRoutes } from "@/app/lib/routes";
-import Breadcrumbs from "@/app/ui/breadcrumbs";
-import EditApplicantForm from "@/app/ui/users/applicants/edit-form";
-import { fetchApplicantById, fetchRoles } from "@/app/lib/requests";
+import UserUpdate from "../../../update";
 
 export const metadata: Metadata = {
-  title: "Edit Applicant",
+  title: "Edit Owner",
 };
 
 export default async function Page({ params }: _IdParams) {
   const id = params.id;
-  const [applicant, roles] = await Promise.all([
-    fetchApplicantById(id),
-    fetchRoles(),
-  ]);
-
-  console.log(applicant);
-
-  if (!applicant) {
-    notFound();
-  }
 
   return (
-    <main>
-      <Breadcrumbs
-        breadcrumbs={[
-          {
-            label: "Applicants",
-            href: `${dashboardRoutes.USERS.APPLICANTS.BASE}`,
-          },
-          {
-            label: "Update Applicant",
-            href: `${dashboardRoutes.USERS.APPLICANTS.BASE}/${id}/update`,
-            active: true,
-          },
-        ]}
-      />
-      <EditApplicantForm applicant={applicant} roles={roles} />
-    </main>
+    <UserUpdate
+      id={id}
+      href={dashboardRoutes.USERS.OWNERS.BASE}
+      label="Owner"
+    />
   );
 }
