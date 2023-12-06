2bf222524b38da25d65eddb7fa9cdeb45d094929
George Acquah <georgeacquah889@gmail.com>
2023-12-06 00:07:40 +0000

    Modified status component to render either is active or is verified, modified pagination component to update the select value based on the usr search params

--- client/src/admin/app/ui/users/status.tsx
+++ client/src/admin/app/ui/users/status.tsx
@@ -2,17 +2,25 @@ import { CheckIcon, XMarkIcon } from "@heroicons/react/24/solid";
 import { Badge } from "@tremor/react";
 
 export default function UserStatus({ status }: { status: string }) {
+  console.log(status)
   return (
-    <span
-    >
+    <span>
       {status === "verified" ? (
         <Badge className="bg-green-500 text-white" icon={CheckIcon}>
           Verified
+        </Badge>
+      ) : status === "available" ? (
+        <Badge className="bg-green-500 text-white" icon={CheckIcon}>
+          Available
         </Badge>
       ) : null}
       {status === "not verified" ? (
         <Badge className="bg-red-500 text-neutral-100 -ml-3" icon={XMarkIcon}>
           Unverified
+        </Badge>
+      ) : status === "not available" ? (
+        <Badge className="bg-red-500 text-neutral-100 -ml-3" icon={XMarkIcon}>
+          Unavailable
         </Badge>
       ) : null}
     </span>
