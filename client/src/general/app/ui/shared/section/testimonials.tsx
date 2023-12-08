b0c859da6e95876c7aa48a2de39843f79bc74eb7
George Acquah <georgeacquah889@gmail.com>
2023-12-08 17:47:44 +0000

    Created created section skeletons for the app and modified the theme toggler component to consume the theme context, as well as added some styles and icons

---
+++ client/src/general/app/ui/shared/section/testimonials.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const TestimonialsSection: React.FC = () => (
+  <section className="testimonials-section py-16 bg-gray-100">
+    <div className="container mx-auto text-center">
+      <h2 className="text-2xl font-bold mb-8">What Our Users Say</h2>
+      {/* Testimonials or reviews */}
+      {/* Example:
+      <div className="max-w-md mx-auto bg-white p-4 mb-4 rounded-md shadow-md">
+        <p className="text-gray-800">"The smart car parking system saved me a lot of time. Highly recommended!"</p>
+        <p className="text-gray-600">- John Doe, Happy User</p>
+      </div>
+      */}
+    </div>
+  </section>
+);
+
+export default TestimonialsSection;
