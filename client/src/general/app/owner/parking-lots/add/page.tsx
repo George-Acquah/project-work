c2f56d1b6adec447bd0fb797e60596dd530bdca1
George Acquah <georgeacquah889@gmail.com>
2024-02-03 05:01:31 +0000

    Created pages to add  and update a center

---
+++ client/src/general/app/owner/parking-lots/add/page.tsx
@@ -0,0 +1,9 @@
+import ParkingCenterForm from "@/app/ui/owner/center-form";
+
+const AddParkingCenterPage = async () => {
+  return (
+    <ParkingCenterForm />
+  )
+}
+
+export default AddParkingCenterPage;
