05117be2a54a5079f322ae6409dd4f610dca32fd
George Acquah <georgeacquah889@gmail.com>
2024-02-03 04:13:57 +0000

    created my root layout and added some svgs

--- client/src/general/app/layout.tsx
+++ client/src/general/app/layout.tsx
@@ -1,12 +1,15 @@
 import type { Metadata } from "next";
 import "@/app/ui/globals.css";
 import { inter } from "@/app/ui/shared/font";
-import { bodyBg } from "./ui/shared/themes";
-import ToggleTheme from "@/app/ui/shared/toggle-theme";
+import { bodyBg } from "@/app/ui/shared/themes";
+import ThemeToggler from "@/app/ui/shared/toggle-theme";
 import { Providers } from "./providers";
 import { themeKey } from "@/utils/contexts/contexts";
 import { cookies } from "next/headers";
-import Header from "./ui/nav/header";
+import Header from "@/app/ui/nav/header";
+import { auth } from "@/auth";
+import AuthModal from "./auth-modal";
+import { getServerSideCookies } from "./lib/actions";
 
 export const metadata: Metadata = {
   title: {
@@ -21,13 +24,16 @@ export default async function RootLayout({ children }: _IChildren) {
   const cookieStore = cookies();
   const themeCookie = cookieStore.get(themeKey);
   const theme = themeCookie?.value;
+
+  const session = await auth();
+
   return (
     <html suppressHydrationWarning lang="en">
       <body className={`${inter.className} antialiased ${bodyBg}`}>
         <Providers theme={theme!}>
-          <Header />
+          <Header loggedIn={ session?.access_token ? true : false} />
           {children}
-          <ToggleTheme />
+          <ThemeToggler />
         </Providers>
       </body>
     </html>
