3b460e4d7b7b7225d84aa2b2fd819f26b6a48c64
George Acquah <georgeacquah889@gmail.com>
2024-02-03 03:48:15 +0000

    created severall endpoints in the center controller and the request reservations logic

---
+++ api/src/parking/dtos/reservation-requests.dto.ts
@@ -0,0 +1,27 @@
+import { IsDate, IsNumber, Min } from 'class-validator';
+
+export class ReservationRequestDto {
+  @IsDate()
+  start_time: Date;
+
+  @IsNumber()
+  @Min(5)
+  reservation_duration: number;
+
+  constructor(dto: ReservationRequestDto) {
+    Object.assign(this, dto);
+  }
+}
+
+export class SlotReservationDto {
+  @IsDate()
+  start_time: Date;
+
+  @IsNumber()
+  @Min(5)
+  reservation_duration: number;
+
+  constructor(dto: SlotReservationDto) {
+    Object.assign(this, dto);
+  }
+}
