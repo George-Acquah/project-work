bff4813597c13b2dda8f9ca002c155f1fec32659
George Acquah <georgeacquah889@gmail.com>
2024-02-03 04:42:50 +0000

    Created layouts for all grouped routes

--- client/src/general/app/driver/layout.tsx
+++ client/src/general/app/driver/layout.tsx
@@ -1,4 +1,11 @@
-const DriverLayout = ({ children }: _IChildren) => {
+import { auth } from "@/auth";
+import { AuthRequiredError } from "../lib/exceptions";
+
+const DriverLayout = async ({ children }: _IChildren) => {
+  const session = await auth();
+  if (!session?.access_token) {
+    
+  }
   return (
     <section className="pb-16 pt-36 md:pb-20 lg:pb-28 lg:pt-[180px]">
       <div className="container">{children}</div>
---
+++ client/src/general/app/owner/parking-lots/layout.tsx
@@ -0,0 +1,9 @@
+const ParkingCentersLayout = async ({ children }: _IChildren) => {
+  return (
+    <section className="">
+      <div className="container">{children}</div>
+    </section>
+  );
+};
+
+export default ParkingCentersLayout;
