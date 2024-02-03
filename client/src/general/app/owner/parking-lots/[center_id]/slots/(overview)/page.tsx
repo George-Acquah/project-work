074d0ec32a4a29daae9a5b26d8330083a781e810
George Acquah <georgeacquah889@gmail.com>
2024-02-03 04:59:25 +0000

    Created slots page of a particular center

---
+++ client/src/general/app/owner/parking-lots/[center_id]/slots/(overview)/page.tsx
@@ -0,0 +1,21 @@
+import React, { Suspense } from "react";
+import ParkingCenters from "@/app/ui/owner/parking-center";
+import MapComp from "@/app/ui/shared/map";
+import List from "@/app/ui/shared/lists";
+import { testCenters } from "@/app/lib/dummy-data";
+import { CardsSkeleton } from "@/app/ui/shared/skeletons";
+import Slots from "@/app/ui/owner/slots";
+
+const ParkingCentersPage = async () => {
+  return (
+    <section className="space-y-8">
+      {/* <List /> */}
+      {/* <Suspense fallback={<CardsSkeleton />}>
+        <MapComp centers={testCenters} highlighted_id="" />
+      </Suspense> */}
+      {/* <Slots /> */}
+    </section>
+  );
+};
+
+export default ParkingCentersPage;
