28f12723e470da59991fa8c7e9f7329ff1793600
George Acquah <georgeacquah889@gmail.com>
2024-05-13 22:48:24 +0000

    Created a root slice for my app's states and a states and types files for my slices types and initial states

---
+++ scp-mobile/features/root.slice.ts
@@ -0,0 +1,52 @@
+import { Center_Filter } from "@/utils/enums/global.enum";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit"
+import { RootState } from "store";
+
+interface _InitialState {
+  showOnboarding: boolean;
+  viewMap: boolean;
+  nearbyMap: boolean;
+  centersFilter: string | null;
+}
+const initialState: _InitialState = {
+  showOnboarding: true,
+  viewMap: false,
+  nearbyMap: true,
+  centersFilter: Center_Filter.AVAILABLE,
+}
+
+const rootSlice = createSlice({
+  name: "root",
+  initialState,
+  reducers: {
+    disableShowOnboarding: (state) => {
+      state.showOnboarding = false;
+    },
+    showMap: (state) => {
+      state.viewMap = true;
+    },
+    collpaseMap: (state) => {
+      state.viewMap = false;
+    },
+    showNearbyMap: (state) => {
+      state.nearbyMap = true;
+    },
+    hideNearbyMap: (state) => {
+      state.nearbyMap = false;
+    },
+    setCentersFilter: (state, action: PayloadAction<string>) => {
+      state.centersFilter = action.payload;
+    }
+  },
+});
+
+export const {disableShowOnboarding, showMap, collpaseMap, hideNearbyMap, showNearbyMap, setCentersFilter } = rootSlice.actions;
+
+export const selectOnboardingStatus = (state: RootState) => state.root.showOnboarding;
+
+export const selectViewMap = (state: RootState) => state.root.viewMap;
+
+export const selectNearbyMap = (state: RootState) => state.root.nearbyMap;
+export const selectCentersFilter = (state: RootState) => state.root.centersFilter;
+
+export default rootSlice.reducer;
---
+++ scp-mobile/features/states.ts
@@ -0,0 +1,89 @@
+import { tabData } from "@/constants/root";
+import { _IAuthState, _IBookingState, _ICenters, _IMapState, _IPermission, _IResrvations, _IRootState, _ISlots, _IVehicles } from "./types";
+
+const rootState: _IRootState = {
+  isLoading: false,
+  error: null,
+  message: null,
+};
+const authInitialState: _IAuthState = {
+  isAuthenticated: false,
+  tokens: null,
+  isLoading: false,
+  message: null,
+  error: null,
+  user: null,
+};
+
+const bookingsInitialState: _IBookingState = {
+  selectedTab: tabData[0],
+  showDetailsModal: false,
+  openMap: false,
+  selectedBooking: null,
+  upcomingBookings: [],
+  bookingHistory: [],
+  favoriteParkingCenters: [],
+  isLoading: false,
+  error: null,
+  message: null,
+};
+
+const centersInitialState: _ICenters = {
+  ...rootState,
+  fetchedCenter: null,
+  selectedCenter: null,
+  availableCenters: [],
+  popularCenters: [],
+  nearbyCenters: [],
+  savedCenter: null,
+  nearbyLoading: false,
+  nearbyError: null,
+  nearbyMessage: null,
+  availableLoading: false,
+  availableError: null,
+  availableMessage: null,
+};
+
+const vehiclesInitialState: _IVehicles = {
+  ...rootState,
+  vehicles: []
+};
+
+const slotsInitialState: _ISlots = {
+  ...rootState,
+  fetchedSlot: null,
+  selectedSlot: null,
+  filteredSlots: [],
+  popularSlots: [],
+  nearbySlot: [],
+  savedSlot: null,
+  nearbyLoading: false,
+  nearbyError: null,
+  nearbyMessage: null,
+};
+
+const initialReservationState: _IResrvations = {
+  ...rootState,
+  availableSlots: null,
+  reservedSlot: null,
+  reservation_loading: false,
+  reservation_error: null,
+  start_time: new Date().toDateString(),
+  duration: 0,
+}
+
+const permissionsInitialState: _IPermission = {
+  ...rootState,
+  currentLocation: null
+}
+
+const mapInitialState: _IMapState = {
+  origin: null,
+  destination: null,
+  travelTimeInformation: null,
+  selected: null,
+};
+
+export {
+  authInitialState, bookingsInitialState, mapInitialState, centersInitialState, permissionsInitialState, slotsInitialState, initialReservationState, vehiclesInitialState
+};
