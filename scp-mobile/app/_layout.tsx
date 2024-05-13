59f346d0d34f4dd26497620340c4e5ec0254cbfb
George Acquah <georgeacquah889@gmail.com>
2024-05-13 16:37:28 +0000

    Modified root layout, defined some constants, and added some mock data

--- scp-mobile/app/_layout.tsx
+++ scp-mobile/app/_layout.tsx
@@ -12,7 +12,12 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    RobotoLight: require("../assets/fonts/Roboto-Light.ttf"),
+    RobotoRegular: require("../assets/fonts/Roboto-Regular.ttf"),
+    RobotoMedium: require("../assets/fonts/Roboto-Medium.ttf"),
+    RobotoBold: require("../assets/fonts/Roboto-Bold.ttf"),
+    RobotoBlack: require("../assets/fonts/Roboto-Black.ttf"),
   });
 
   useEffect(() => {
