4b0ea547c51100098d8ed969f85ecf6699d6d2ef
George Acquah <georgeacquah889@gmail.com>
2024-05-13 22:52:51 +0000

    Created slices for bookings, parking centers, parking slots, reservations and vehicles

---
+++ scp-mobile/features/bookings/bookings.slice.ts
@@ -0,0 +1,123 @@
+import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { bookingsInitialState } from "../states";
+import { RootState } from "store";
+
+// This will be for the actual implementation
+interface _IBookingUrls {
+  upcomingUrl: string;
+  historyUrl: string;
+  favouriteUrl: string;
+}
+
+// For testing
+interface _IBookingData {
+  upcomingUrl: _IBooking[];
+  historyUrl: _IBooking[];
+  favouriteUrl: _IFavoriteParkingCenter[];
+}
+
+export function fetchData<T = any>(data: T, timeout: number) {
+  return new Promise<T>((resolve) => {
+    setTimeout(() => {
+      resolve(data);
+    }, timeout);
+  });
+}
+
+export const fetchAllBookings = createAsyncThunk(
+  "bookings/fetchAllBookings",
+  async (bookingUrls: _IBookingData) => {
+    try {
+      const { upcomingUrl, historyUrl, favouriteUrl } = bookingUrls;
+
+      const [upcomingResponse, historyResponse, favoritesResponse] =
+        await Promise.all([
+          fetchData<_IBooking[]>(upcomingUrl, 300),
+          fetchData<_IBooking[]>(historyUrl, 300),
+          fetchData<_IFavoriteParkingCenter[]>(favouriteUrl, 300),
+        ]);
+
+      return {
+        upcomingBookings: upcomingResponse,
+        bookingHistory: historyResponse,
+        favoriteParkingCenters: favoritesResponse,
+      };
+    } catch (error) {
+      throw error;
+    }
+  }
+);
+
+const bookingSlice = createSlice({
+  name: "booking",
+  initialState: bookingsInitialState,
+  reducers: {
+    setSelectedTab: (state, action: PayloadAction<string>) => {
+      state.selectedTab = action.payload;
+    },
+    handleOpenMap: (state) => {
+      state.openMap = true;
+    },
+
+    handleCloseMap: (state) => {
+      state.openMap = false;
+    },
+
+    setShowDetailsModal: (state, action: PayloadAction<boolean>) => {
+      state.openMap = action.payload;
+    },
+    handleShowDetailsModal: (
+      state,
+      action: PayloadAction<_IBooking | _IFavoriteParkingCenter>
+    ) => {
+      state.selectedBooking = action.payload;
+      state.showDetailsModal = true;
+    },
+    handleCloseDetailsModal: (state) => {
+      state.selectedBooking = null;
+      state.showDetailsModal = false;
+      state.openMap = false;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAllBookings.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(fetchAllBookings.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.upcomingBookings = action.payload.upcomingBookings;
+        state.bookingHistory = action.payload.bookingHistory;
+        state.favoriteParkingCenters = action.payload.favoriteParkingCenters;
+      })
+      .addCase(fetchAllBookings.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message;
+      });
+  },
+});
+
+export const { setSelectedTab, handleOpenMap, handleCloseMap, handleCloseDetailsModal, handleShowDetailsModal } = bookingSlice.actions;
+
+export const selectSelctedTab = (state: RootState) => state.booking.selectedTab;
+
+export const selectOpenMap = (state: RootState) => state.booking.openMap;
+
+export const selectShowDetailsModal = (state: RootState) => state.booking.showDetailsModal
+
+export const selectSelectedBooking = (state: RootState) => state.booking.selectedBooking;
+
+export const selectUpcomingBookings = (state: RootState) =>
+  state.booking.upcomingBookings;
+
+export const selectBookingsHistory = (state: RootState) =>
+  state.booking.bookingHistory;
+
+export const selectFavoriteCenters = (state: RootState) =>
+  state.booking.favoriteParkingCenters;
+
+export const selectBookingLoading = (state: RootState) =>
+  state.booking.isLoading;
+
+export default bookingSlice.reducer;
---
+++ scp-mobile/features/vehicles/vehicles.slice.ts
@@ -0,0 +1,97 @@
+import {
+  PayloadAction,
+  createAsyncThunk,
+  createSelector,
+  createSlice,
+} from "@reduxjs/toolkit";
+import { vehiclesInitialState } from "../states";
+import { allVehicles } from "@/api/vehicles";
+import { RootState } from "@/store";
+
+export const fetchAllVehicles = createAsyncThunk(
+  "center/fetchAllVehicles",
+  async (centerParams: _IVehicleParams) => {
+    try {
+      const { vehicles = "", currentPage = 1, pageSize = 5 } = centerParams;
+      const response = await allVehicles(vehicles, currentPage, pageSize);
+      console.log(response);
+      return response;
+    } catch (error) {
+      throw error;
+    }
+  }
+);
+
+// export const fetchSingleVehicle = createAsyncThunk(
+//   "center/fetchSingleVehicle",
+//   async (center_id: string) => {
+//     try {
+//       const response = await singleCenter(center_id);
+
+//       return response;
+//     } catch (error) {
+//       throw error;
+//     }
+//   }
+// );
+
+const vehicleSlice = createSlice({
+  name: "vehicles",
+  initialState: vehiclesInitialState,
+  reducers: {
+    // setSelectedVehicle: (state, action: PayloadAction<_IParkingCenter>) => {
+    //   state.selectedCenter = action.payload;
+    // },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAllVehicles.fulfilled, (state, action) => {
+        // state.popularCenters = action.payload.data;
+        state.vehicles = action.payload.data
+        state.isLoading = false;
+        state.message = action.payload.message;
+        state.error = null;
+      })
+      .addCase(fetchAllVehicles.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(fetchAllVehicles.rejected, (state, action) => {
+        state.isLoading = false;
+        state.message = null;
+        state.error = action.error.message!;
+      })
+
+      // .addCase(fetchSingleVehicle.fulfilled, (state, action) => {
+      //   state.fetchedCenter = action.payload.data;
+      //   state.isLoading = false;
+      //   state.message = action.payload.message;
+      //   state.error = null;
+      // })
+      // .addCase(fetchSingleVehicle.pending, (state) => {
+      //   state.isLoading = true;
+      //   state.error = null;
+      // })
+      // .addCase(fetchSingleVehicle.rejected, (state, action) => {
+      //   state.isLoading = false;
+      //   state.message = null;
+      //   state.error = action.error.message;
+      // });
+  },
+});
+
+export const { } = vehicleSlice.actions;
+
+export const selectAllVehicles = (state: RootState) =>
+  state.vehicle.vehicles;
+
+export const selectMemoedVehicles = createSelector(
+  [selectAllVehicles],
+  (vehicles) => vehicles.map((vehicle) => vehicle._id)
+);
+
+export const selectVehiclesLoading = (state: RootState) =>
+  state.vehicle.isLoading;
+export const selectVehiclesError = (state: RootState) => state.vehicle.error;
+
+export default vehicleSlice.reducer;
