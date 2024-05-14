11aa9e74b859126d422ead0031c044a1555cfab6
George Acquah <georgeacquah889@gmail.com>
2024-05-14 07:46:44 +0000

    Created a useRedux hook to minimise usage of AppStore in my codes and added some enums

---
+++ scp-mobile/utils/enums/global.enum.ts
@@ -0,0 +1,38 @@
+enum SlotTypes {
+  TYPE_A = "Class A",
+  TYPE_B = "Class B",
+  TYPE_C = "Class C",
+}
+
+enum CenterTypes {
+  TYPE_A = "Class A",
+  TYPE_B = "Class B",
+  TYPE_C = "Class C",
+}
+
+enum Center_Type {
+  NEARBY = "nearby-centers",
+  POPULAR = "popular-centers",
+}
+
+enum Slot_Type {
+  NEARBY = "nearby-slots",
+  POPULAR = "popular-slots"
+}
+
+enum Merged_Types {
+  NEARBY_CENTER = "nearby-centers",
+  POPULAR_CENTER = "popular-centers",
+  NEARBY_SLOT = "nearby-slots",
+  POPULAR_SLOT= "popular-slots",
+}
+
+enum Center_Filter {
+  AVAILABLE = "Available",
+  POPULAR = "Popular",
+  NEARBY = "Nearby"
+}
+
+export {
+  Center_Type, Slot_Type, Merged_Types, Center_Filter
+}
