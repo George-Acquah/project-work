a139decacb8475c1397db38279ca6a3c252cd51d
George Acquah <georgeacquah889@gmail.com>
2024-05-14 08:00:57 +0000

    modified tabs layout

--- scp-mobile/app/(tabs)/_layout.tsx
+++ scp-mobile/app/(tabs)/_layout.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { TabBarIcon } from '@/components/navigation/TabBarIcon';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -19,7 +20,7 @@ export default function TabLayout() {
         options={{
           title: 'Home',
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+            <TabBarIcon fontProvider={Ionicons} name={focused ? 'home' : 'home-outline'} color={color} />
           ),
         }}
       />
@@ -28,7 +29,7 @@ export default function TabLayout() {
         options={{
           title: 'Explore',
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+            <TabBarIcon fontProvider={Ionicons} name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
           ),
         }}
       />
