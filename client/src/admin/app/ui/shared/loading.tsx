f6f47418073ab4471e23e840e3a01923ca37dd44
George Acquah <georgeacquah889@gmail.com>
2024-06-20 15:50:27 +0000

    updated constants

--- client/src/admin/app/ui/shared/loading.tsx
+++ client/src/admin/app/ui/shared/loading.tsx
@@ -1,13 +1,16 @@
 import React from "react";
+import { inter } from "../font";
+import { strongTextColor } from "../themes";
 
-const Loading = ({bg = 'blue-500'}: {bg?: string}) => {
+const Loading = ({ bg = "blue-500", label }: { bg?: string; label?: string }) => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-white bg-opacity-80 z-50">
+    <div className="fixed inset-0 flex flex-col items-center justify-center bg-white bg-opacity-80 z-50">
       <div className="flex space-x-4">
-        <div className={`w-5 h-5 bg-${bg} rounded-full animate-bounce1`}/>
-        <div className={`w-5 h-5 bg-${bg} rounded-full animate-bounce2`}/>
-        <div className={`w-5 h-5 bg-${bg} rounded-full animate-bounce3`}/>
+        <div className={`w-5 h-5 bg-${bg} rounded-full animate-bounce1`} />
+        <div className={`w-5 h-5 bg-${bg} rounded-full animate-bounce2`} />
+        <div className={`w-5 h-5 bg-${bg} rounded-full animate-bounce3`} />
       </div>
+      {label && <h1 className={`${inter.className} text-2xl my-8 ${strongTextColor}`}>{label}</h1>}
     </div>
   );
 };
