ab5caab3376f777f39cd9751c54d7e4653ff7bde
George Acquah <georgeacquah889@gmail.com>
2024-06-23 21:34:53 +0000

    new changes

--- client/src/admin/app/ui/modals/error.modal.tsx
+++ client/src/admin/app/ui/modals/error.modal.tsx
@@ -4,10 +4,13 @@ import React from "react";
 import { DialogPanel, DialogTitle } from "@headlessui/react";
 import useCustomSearchParams from "@/hooks/search-params.hook";
 import { ExclamationCircleIcon } from "@heroicons/react/24/outline";
-import Modal from "../modal";
+import Modal from "../shared/modal";
 
 const ErrorModal = () => {
-  const { handleSetParams, modalValue,paramValues } = useCustomSearchParams("ERROR", ['ERR_MSG', 'BTN_LABEL','ERR_DESC']);
+  const { handleSetParams, modalValue, paramValues } = useCustomSearchParams(
+    "ERROR",
+    ["ERR_MSG", "BTN_LABEL", "ERR_DESC"]
+  );
   const modal = !!modalValue;
   const error_message = paramValues.ERR_MSG;
   const error_description = paramValues.ERR_DESC;
@@ -18,7 +21,7 @@ const ErrorModal = () => {
       onClick={() => handleSetParams(false)}
       className="inline-flex items-center gap-2 rounded-md bg-slate-800/70 dark:bg-gray-700 py-1.5 px-3 text-sm font-semibold text-white shadow-inner shadow-white/10 focus:outline-none hover:bg-gray-600"
     >
-      { button_label ?? 'Close'}
+      {button_label ?? "Close"}
     </button>
   );
 
@@ -35,12 +38,15 @@ const ErrorModal = () => {
           </DialogTitle>
         </div>
         <p className="mt-4 text-sm text-gray-700 dark:text-white/70">
-          { error_message ?? `An unexpected error occurred. Please try again later or contact
+          {error_message ??
+            `An unexpected error occurred. Please try again later or contact
           support if the issue persists.`}
         </p>
-        { error_description && <p className="mt-2 text-sm text-gray-700 dark:text-white/70">
-          { error_description }
-        </p>}
+        {error_description && (
+          <p className="mt-2 text-sm text-gray-700 dark:text-white/70">
+            {error_description}
+          </p>
+        )}
         <div className="mt-12 flex justify-end">
           <CloseButton />
         </div>
