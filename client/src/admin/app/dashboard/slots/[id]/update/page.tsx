719c46bce0b9961ad2cffce0050325cccba19238
George Acquah <georgeacquah889@gmail.com>
2024-06-30 10:43:24 +0000

    created dynamic slot pages and adding of new slot pages

---
+++ client/src/admin/app/dashboard/slots/[id]/update/page.tsx
@@ -0,0 +1,29 @@
+import { notFound } from "next/navigation";
+
+import { Metadata } from "next";
+import { fetchCenterById } from "@/app/lib/requests";
+import Breadcrumbs from "@/app/ui/shared/breadcrumbs";
+import CENTERS_BREADCRUMBS from "@/constants/centers.constants";
+import UpdateCenter from "@/app/ui/centers/update-center";
+import { dashboardRoutes } from "@/app/lib/routes";
+
+export const metadata: Metadata = {
+  title: "Edit Center",
+};
+
+export default async function Page({ params }: _IdParams) {
+  const id = params.id;
+  const center = await fetchCenterById(id);
+
+  if (!center) {
+    notFound();
+  }
+
+  return (
+    <main>
+      <Breadcrumbs breadcrumbs={CENTERS_BREADCRUMBS(id).UPDATE_CENTER} />
+      <h1 className="text-center text-3xl">{JSON.stringify(center)}</h1>
+      <UpdateCenter id={id} label={"Center"} href={dashboardRoutes.PARKING_LOTS.BASE} formType={"group"} />
+    </main>
+  );
+}
