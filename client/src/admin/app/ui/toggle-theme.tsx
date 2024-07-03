50be1028ccc25f9651534b6816dabbeb1e30889b
George Acquah <georgeacquah889@gmail.com>
2024-07-03 02:53:40 +0000

    refactored components

--- client/src/admin/app/ui/toggle-theme.tsx
+++ client/src/admin/app/ui/toggle-theme.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { bgColor } from "./themes";
 import { useTheme } from "../../utils/contexts/theme.context";
-import { SvgSun, SvgMoon } from "./icons";
+import { SvgSun, SvgMoon } from "./shared/icons";
 const themeClass = `fixed bottom-4 right-4 cursor-pointer flex items-center justify-center rounded-full z-50 w-8 h-8 ${bgColor}`;
 
 const ThemeToggler = () => {
