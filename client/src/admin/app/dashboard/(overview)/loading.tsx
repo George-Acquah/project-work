5565b72343b20ed294cb09d6e2fdb386a4cc0105
George Acquah <georgeacquah889@gmail.com>
2024-07-03 02:54:37 +0000

    refactored components

--- client/src/admin/app/dashboard/(overview)/loading.tsx
+++ client/src/admin/app/dashboard/(overview)/loading.tsx
@@ -1,4 +1,4 @@
-import { DashboardMainSkeleton } from "@/app/ui/skeletons";
+import { DashboardMainSkeleton } from "@/app/ui/shared/skeletons";
 
 export default function Loading() {
   return <DashboardMainSkeleton />;
