45ece193aec2f3f1a4dabfc31c48b092d67415b2
George Acquah <georgeacquah889@gmail.com>
2024-07-28 21:54:19 +0000

    new changes

--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(number_plate: string): boolean {
+    console.log(number_plate);
+    return true;
   }
 }
